1862bc54e2665b755e8a26a7c557a4152cf2bf55
Glitch-dm7 <divyadarshandvd007@gmail.com>
2024-11-13 21:29:37 +0530

    Cloudinary + Multer Setup

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -46,6 +46,7 @@ const userSchema = new Schema({
   }
 }, {timestamps : true})
 
+// Middleware to hash the password before saving the user document if the password was modified
 userSchema.pre("save", async function (next){
   if(!this.isModified("password")) return next();
 
@@ -53,10 +54,12 @@ userSchema.pre("save", async function (next){
   next()
 })
 
+// Method to check if the provided password matches the stored hashed password
 userSchema.methods.isPasswordCorrect = async function (password){
   return await bcrypt.compare(password, this.password)
 }
 
+// Method to generate an access token for the user
 userSchema.methods.generateAccessToken = function(){
   jwt.sign(
     {
@@ -72,6 +75,7 @@ userSchema.methods.generateAccessToken = function(){
   )
 }
 
+// Method to generate a refresh token for the user
 userSchema.methods.generateRefreshToken = function(){
   jwt.sign(
     {
